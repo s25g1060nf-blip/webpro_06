bdbb884357b0b15942dd2e58a2bbe055b83c8fea
s25g1060nf-blip <s25g1060nf@s.chibakoudai.jp>
2025-12-26 08:20:33 +0900

    Update app5.js

--- app5.js
+++ app5.js
@@ -23,63 +23,95 @@ app.get("/omikuji1", (req, res) => {
   let luck = '';
   if( num==1 ) luck = '大吉';
   else if( num==2 ) luck = '中吉';
+  else if( num==3 ) luck = '小吉';
+  else if( num==4 ) luck = '末吉';
+  else if( num==5 ) luck = '凶';
+  else if( num==6 ) luck = '大凶';
 
   res.send( '今日の運勢は' + luck + 'です' );
 });
+
 
 app.get("/omikuji2", (req, res) => {
   const num = Math.floor( Math.random() * 6 + 1 );
   let luck = '';
   if( num==1 ) luck = '大吉';
   else if( num==2 ) luck = '中吉';
+  else if( num==3 ) luck = '小吉';
+  else if( num==4 ) luck = '末吉';
+  else if( num==5 ) luck = '凶';
+  else if( num==6 ) luck = '大凶';
 
   res.render( 'omikuji2', {result:luck} );
 });
 
 app.get("/janken", (req, res) => {
   let hand = req.query.hand;
-  let win = Number( req.query.win );
-  let total = Number( req.query.total );
-  console.log( {hand, win, total});
-  const num = Math.floor( Math.random() * 3 + 1 );
+  let win = Number(req.query.win);
+  let total = Number(req.query.total);
+  console.log({ hand, win, total });
+
+  const num = Math.floor(Math.random() * 3 + 1);
   let cpu = '';
   let judgement = '';
-  if( num==1 ) cpu = 'グー';
-  else if( num==2 ) cpu = 'チョキ';
+
+  if (num == 1) cpu = 'グー';
+  else if (num == 2) cpu = 'チョキ';
   else cpu = 'パー';
-  // ここに勝敗の判定を入れる
-  // 以下の数行は人間の勝ちの場合の処理なので，
-  // 判定に沿ってあいこと負けの処理を追加する
-  judgement = '勝ち';
-  win += 1;
-  total += 1;
+
+  // 勝敗の判定
+  if (
+    (hand === 'グー' && cpu === 'チョキ') ||
+    (hand === 'チョキ' && cpu === 'パー') ||
+    (hand === 'パー' && cpu === 'グー')
+  ) {
+    // 人間の勝ち
+    judgement = '勝ち';
+    win += 1;
+    total += 1;
+  } else if (hand === cpu) {
+    // あいこ（試合数はカウントしない）
+    judgement = 'あいこ';
+  } else {
+    // 人間の負け
+    judgement = '負け';
+    total += 1;
+  }
+
   const display = {
     your: hand,
     cpu: cpu,
     judgement: judgement,
     win: win,
     total: total
-  }
-  res.render( 'janken', display );
+  };
+
+  res.render('janken', display);
 });
 
-let station = [
-  { id:1, code:"JE01", name:"東京駅"},
-  { id:2, code:"JE07", name:"舞浜駅"},
-  { id:3, code:"JE12", name:"新習志野駅"},
-  { id:4, code:"JE13", name:"幕張豊砂駅"},
-  { id:5, code:"JE14", name:"海浜幕張駅"},
-  { id:6, code:"JE05", name:"新浦安駅"},
+
+let station2 = [
+  { id:1, code:"JE01", name:"東京駅", change:"総武本線，中央線，etc", passengers:403831, distance:0 },
+  { id:2, code:"JE02", name:"八丁堀駅", change:"日比谷線", passengers:31071, distance:1.2 },
+  { id:3, code:"JE05", name:"新木場駅", change:"有楽町線，りんかい線", passengers:67206, distance:7.4 },
+  { id:4, code:"JE07", name:"舞浜駅", change:"舞浜リゾートライン", passengers:76156,distance:12.7 },
+  { id:5, code:"JE12", name:"新習志野駅", change:"", passengers:11655, distance:28.3 },
+  { id:6, code:"JE17", name:"千葉みなと駅", change:"千葉都市モノレール", passengers:16602, distance:39.0 },
+  { id:7, code:"JE18", name:"蘇我駅", change:"内房線，外房線", passengers:31328, distance:43.0 },
 ];
 
-app.get("/keiyo_add", (req, res) => {
-  let id = req.query.id;
-  let code = req.query.code;
-  let name = req.query.name;
-  let newdata = { id: id, code: code, name: name };
-  station.push( newdata );
-  res.render('db1', { data: station });
+app.get("/keiyo2", (req, res) => {
+  const result = station2;
 
+  res.render('keiyo2', { data: result });
 });
+
+app.get("/keiyo2/:number", (req, res) => {
+  const number = req.params.number;
+  const detail = station2[number];
+
+  res.render('keiyo2_detail', { data: detail });
+});
+
 
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
